b4f8ac6e7335f8f6e137092efd933754891fad0e
Caiovsscodes <caiovictorfamilia14@gmail.com>
2025-06-26 16:15:52 -0300

    TCC

--- clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Repository/Historico_medicoRepository.java
+++ clinicaMedica/clinicaMedica/src/main/java/com/API/clinicaMedica/Repository/Historico_medicoRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.API.clinicaMedica.Model.Historico_medicoModel;
-
 @Repository
 public interface Historico_medicoRepository extends JpaRepository<Historico_medicoModel, Long>{
     
